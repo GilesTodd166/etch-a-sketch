ec2959b9669d60b8edca57987a10dee56a3f1285
gilestodd166 <167215806+GilesTodd166@users.noreply.github.com>
2025-04-10 16:52:52 -0400

    Added RGB functionality.
    Added styling for aesthetics.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # etch-a-Sketch
 Browser based Etch-a-Sketch game.
+
+Using Javascript DOM manipulation and CSS to complete.
--- scripts.js
+++ scripts.js
@@ -1,32 +1,46 @@
-let userInput = 16;
-let cellsTotal = (userInput * userInput);
+function updateGridSize(userInput) {
+    let cellsTotal = (userInput * userInput);
+    const grid = document.getElementById('grid');
+        for (i = 0; i < cellsTotal; i++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.backgroundColor = 'white';
+            cell.style.width = `${640/ userInput}px`;
+            cell.style.height = `${640/ userInput}px`;
+        
+            grid.appendChild(cell);
 
-const grid = document.getElementById('grid');
-    for (i = 0; i < cellsTotal; i++) {
-      const cell = document.createElement('div');
-      cell.classList.add('cell');
-      grid.appendChild(cell);
+            cell.addEventListener('mouseover', (e) => {
+                
+                function randomColor() {
+                    var red= " ";
+                    var green = '';
+                    var blue = '';
+                    var rgbColor = " ";
+            
+                    red = Math.floor(Math.random() * 256 );
+                    green = Math.floor(Math.random() * 256 );
+                    blue = Math.floor(Math.random() * 256 );
+                    
+                    rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+                    return rgbColor;
+                }
+                    e.target.style.background = randomColor();
+            });
     };
+};
 
-// Set hover affect to change background color when mouse over occurs.
-
-const mouse = document.querySelectorAll('.cell');
-mouse.forEach((cell) => {
-    cell.addEventListener('mouseover', (e) => {
-        e.target.style.background = '#ccc'
-    });
-});
-
-/* Add button to ask for user prompt to enter new grid size to to 100x100 limit.
-Reset grid to new size and refresh tiles.
-
-*/
+updateGridSize(16);
 
 function gridPrompt() {
-    userInput = prompt("Enter new dimensions for the grid, for example, enter 36 to generate a 36x36 grid.");
+    let userInput = prompt("Enter new dimensions for the grid, for example, enter 36 to generate a 36x36 grid.");
+        while (isNaN(userInput) || userInput > 100 || userInput < 1) {
+            userInput = prompt("Enter new dimensions between 1 and 100.");
+        }
+    grid.innerHTML = '';
+    updateGridSize(userInput);
 };
 
 const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', gridPrompt);
-
